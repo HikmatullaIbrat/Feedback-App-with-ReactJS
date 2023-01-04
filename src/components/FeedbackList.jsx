9a06360522434caabd5c47bcfa8956ad3dd7b081
unknown <hikmatullah.ibrat2018@gmail.com>
2023-01-04 11:18:20 +0430

    animation added successfully

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
+import {motion, AnimatePresence} from 'framer-motion'
 function FeedbackList({feedback, handleDelete}) {
     // console.log(feedback)
     if(!feedback || feedback.length === 0){
@@ -8,10 +9,22 @@ function FeedbackList({feedback, handleDelete}) {
     }
   return (
 
-    <div className='feedback-list'>{feedback.map((item)=>(
-        // <FeedbackItem key={item.id} item={item} handleDelete={(id) => console.log(id)} />
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-    ))}</div>
+    // <div className='feedback-list'>{feedback.map((item)=>(
+    //     // <FeedbackItem key={item.id} item={item} handleDelete={(id) => console.log(id)} />
+    //     <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+    // ))}</div>
+
+    // The animated version
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((item)=>(
+          <motion.div key={item.id} initial={{opacity:0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+            {/* <FeedbackItem key={item.id} item={item} handleDelete={(id) => console.log(id)} /> */}
+            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
   )
 
 }
